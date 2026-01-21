c6af0bbf1d8d73798975aacdb6bef4335fe4892e
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-01-20 20:48:23 -0800

    Add getRandomChar function to Password component for enhanced password generation

--- src/Password.jsx
+++ src/Password.jsx
@@ -4,6 +4,11 @@ function random(n) {
   return Math.floor(Math.random() * n)
 }
 
+function getRandomChar() {
+  const chars = '!@#$%^&*()_+-=[]{}|;:,.<>?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  return chars[random(chars.length)]
+}
+
 function generatePassword() {
   // generate a password here
   console.log("generating password")
