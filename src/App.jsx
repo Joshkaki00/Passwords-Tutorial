b3e69c9542f2d571f4b1e4af32dc93f743f782f8
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-01-20 21:44:29 -0800

    Add header and integrate PasswordsList component into App for improved user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Password from './Password';
+import PasswordsList from './PasswordsList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Password Generator</h1>
       <Password />
+      <PasswordsList />
     </div>
   );
 }
